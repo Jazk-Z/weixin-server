4232c31c20dd225624e9644a62fbdbcafd1af963
pandaCure <zzyamor@163.com>
2020-04-23 01:24:37 +0800

    feat:add git webhook interface test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const Router = require('koa-router')
-const router = new Router()
 const gitHookRoutes = require('./git-hook')
+const weixinRoutes = require('./weixin')
+const router = new Router()
 router.use(gitHookRoutes.routes())
+router.use(weixinRoutes.routes())
 module.exports = router
