2ce86255d06f7a30b9dcd8d4a45501ecad121004
pandaCure <zzyamor@163.com>
2020-04-23 15:07:42 +0800

    feat: test git hook

--- routes/git-hook/index.js
+++ routes/git-hook/index.js
@@ -4,6 +4,10 @@ const router = new Router({
 })
 router.post('/push', (ctx, next) => {
   console.log('method Post', JSON.stringify(ctx, null, '\n'))
+  console.log(ctx.body)
+  ctx.body = {
+    ok: true
+  }
 })
 router.get('/push', (ctx, next) => {
   console.log('method GET', JSON.stringify(ctx, null, '\n'))
